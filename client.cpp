b8aa8e8df74f28cf74a293316ff7bb98d5fb081f
HYHsiao0625 <as12345521117@gmail.com>
2024-05-30 01:45:08 +0800

    server & client forward_pass-convolution & sigmoid & maxpooling

--- client.cpp
+++ client.cpp
@@ -20,8 +20,42 @@ using namespace std::chrono;
 const char* host = "0.0.0.0";
 int port = 7000;
 
+const int filter_size = 5;
+const double eta = 0.01;
+const int batch_size = 200;
+
+/* ************************************************************ */
+
 vector<vector<int>> data_train(60000, vector<int>(784, 0));
+vector<vector<int>> data_test(10000, vector<int>(784, 0));
 vector<int> label_train(60000, 0);
+vector<int> label_test(10000, 0);
+
+vector<vector<vector<double>>> enc_conv_w(8, vector<vector<double>>(5, vector<double>(5, 0)));
+vector<vector<vector<double>>> enc_conv_b(8, vector<vector<double>>(28, vector<double>(28, 0)));
+vector<vector<vector<double>>> enc_conv_layer(8, vector<vector<double>>(28, vector<double>(28, 0)));
+vector<vector<vector<double>>> enc_sig_layer(8, vector<vector<double>>(28, vector<double>(28, 0)));
+vector<vector<vector<double>>> max_pooling(8, vector<vector<double>>(28, vector<double>(28, 0)));
+vector<vector<vector<double>>> max_layer(8, vector<vector<double>>(14, vector<double>(14, 0)));
+
+vector<double> dense_input(1568, 0);
+vector<vector<double>> dense_w(1568, vector<double>(120, 0));
+vector<double> dense_b(120, 0);
+vector<double> dense_sum(120, 0);
+vector<double> dense_sigmoid(120, 0);
+vector<vector<double>> dense_w2(120, vector<double>(10, 0));
+vector<double> dense_b2(10, 0);
+vector<double> dense_sum2(10, 0);
+vector<double> dense_softmax(10, 0);
+
+vector<vector<double>> dw2(120, vector<double>(10, 0));
+vector<double> db2(10, 0);
+vector<vector<double>> dw1(1568, vector<double>(120, 0));
+vector<double> db1(120, 0);
+
+vector<vector<vector<double>>> dw_max(8, vector<vector<double>>(28, vector<double>(28, 0)));
+vector<vector<vector<double>>> dw_conv(8, vector<vector<double>>(5, vector<double>(5, 0)));
+vector<vector<vector<double>>> db_conv(8, vector<vector<double>>(28, vector<double>(28, 0)));
 
 /* ************************************************************ */
 
@@ -32,6 +66,65 @@ double sigmoid(double x)
 	return 1 / (1 + exp(-x));
 }
 
+double d_sigmoid(double x) 
+{
+	double sig = sigmoid(x);
+	return sig * (1 - sig);
+}
+
+double softmax_den(vector<double> x, int len) 
+{
+	double val = 0;
+	for (int i = 0; i < len; i++) 
+	{
+		val += exp(x[i]);
+	}
+	return val;
+}
+
+void initialise_weights() 
+{
+	for (int i = 0; i < 8; i++) 
+	{
+		for (int j = 0; j < 28; j++) 
+		{
+			for (int k = 0; k < 28; k++) 
+			{
+				if (j < 5 && k < 5) 
+				{
+					enc_conv_w[i][j][k] = 2 * double(rand()) / RAND_MAX - 1; // Random double value between -1 and 1
+				}
+				enc_conv_b[i][j][k] = 2 * double(rand()) / RAND_MAX - 1; // Random double value between -1 and 1
+			}
+		}
+	}
+
+	for (int i = 0; i < 1568; i++) 
+	{
+		for (int j = 0; j < 120; j++) 
+		{
+			dense_w[i][j] = 2 * double(rand()) / RAND_MAX - 1;
+		}
+	}
+
+	for (int i = 0; i < 120; i++) 
+	{
+		dense_b[i] = 2 * double(rand()) / RAND_MAX - 1;
+	}
+
+	for (int i = 0; i < 120; i++) 
+	{
+		for (int j = 0; j < 10; j++) 
+		{
+			dense_w2[i][j] = 2 * double(rand()) / RAND_MAX - 1;
+		}
+	}
+
+	for (int i = 0; i < 10; i++) 
+	{
+		dense_b2[i] = 2 * double(rand()) / RAND_MAX - 1;
+	}
+}
 /* ************************************************************ */
 
 void read_train_data()
@@ -130,6 +223,7 @@ int main()
     }
 	
 	read_train_data();
+	initialise_weights();
 	
 	vector<vector<int>> img(32, vector<int>(32, 0));
 	vector<int> vector_y(10, 0);
@@ -152,6 +246,72 @@ int main()
 			memset(outdata, 0, sizeof(outdata));
 		} while (data_size > 0);
 		
+		k = 0;
+		data_size = 8 * 5 * 5;
+		do
+		{
+			sprintf(outdata, "%f", enc_conv_w[k / 25][(k / 5) % 5][k % 5]);
+			send(clientSocket, outdata, sizeof(outdata), 0);
+			data_size--;
+			k++;
+			memset(outdata, 0, sizeof(outdata));
+		} while (data_size > 0);
+
+		data_size = 8 * 28 * 28;
+		k = 0;
+		do
+		{
+			sprintf(outdata, "%f", enc_conv_b[k / 784][(k / 28) % 28][k % 28]);
+			send(clientSocket, outdata, sizeof(outdata), 0);
+			data_size--;
+			k++;
+			memset(outdata, 0, sizeof(outdata));
+		} while (data_size > 0);
+
+		data_size = 8 * 28 * 28;
+		k = 0;
+		do
+		{
+			int nbytes = recv(clientSocket, indata, sizeof(indata), 0);
+			if (nbytes <= 0) 
+			{
+				close(clientSocket);
+				printf("client closed connection.\n");
+				break;
+			}
+			enc_conv_layer[k / 784][(k / 28) % 28][k % 28] = atof(indata);
+			data_size--;
+			k++;
+			memset(indata, 0, sizeof(indata));
+		} while (data_size > 0);
+
+		for (int filter_dim = 0; filter_dim < 8; filter_dim++) 
+		{
+			for (int i = 0; i < 28; i++) 
+			{
+				for (int j = 0; j < 28; j++) 
+				{
+					//decryption
+
+					//sigmoid
+					enc_sig_layer[filter_dim][i][j] = sigmoid(enc_conv_layer[filter_dim][i][j]);
+				
+					//encryption
+				}
+			}
+		}
+
+		data_size = 8 * 28 * 28;
+		k = 0;
+		do
+		{
+			sprintf(outdata, "%f", enc_sig_layer[k / 784][(k / 28) % 28][k % 28]);
+			send(clientSocket, outdata, sizeof(outdata), 0);
+			data_size--;
+			k++;
+			memset(outdata, 0, sizeof(outdata));
+		} while (data_size > 0);
+		
 		auto endTime = chrono::high_resolution_clock::now();
 		chrono::duration<double, milli> elapsed = endTime - startTime;
 		double elapsedSeconds = elapsed.count() / 1000.0;
@@ -159,7 +319,7 @@ int main()
 		int elapsedSeconds_ = static_cast<int>(elapsedSeconds) % 60;
 		int elapsedMilliseconds = static_cast<int>(elapsedSeconds * 1000) % 1000;
 		cout << fixed << setprecision(10);
-		cout << "Training time : " << elapsedMinutes << "m " << elapsedSeconds_ << "s " << elapsedMilliseconds << "ms";
+		cout << "Transmission time : " << elapsedMinutes << "m " << elapsedSeconds_ << "s " << elapsedMilliseconds << "ms";
 		cout << endl;
 
 		int nbytes = recv(clientSocket, indata, sizeof(indata), 0);
--- server.cpp
+++ server.cpp
@@ -29,6 +29,43 @@ int main()
 	double in_data = 0;
     double a = 0, b = 0;
     int on = 1;
+
+    /* ************************************************************ */
+
+    const int filter_size = 5;
+    const double eta = 0.01;
+    const int batch_size = 200;
+
+    /* ************************************************************ */
+
+    vector<vector<vector<double>>> enc_conv_w(8, vector<vector<double>>(5, vector<double>(5, 0)));
+    vector<vector<vector<double>>> enc_conv_b(8, vector<vector<double>>(28, vector<double>(28, 0)));
+    vector<vector<vector<double>>> enc_conv_layer(8, vector<vector<double>>(28, vector<double>(28, 0)));
+    vector<vector<vector<double>>> enc_enc_sig_layer(8, vector<vector<double>>(28, vector<double>(28, 0)));
+    vector<vector<vector<double>>> enc_max_pooling(8, vector<vector<double>>(28, vector<double>(28, 0)));
+    vector<vector<vector<double>>> enc_max_layer(8, vector<vector<double>>(14, vector<double>(14, 0)));
+
+    vector<double> enc_dense_input(1568, 0);
+    vector<vector<double>> enc_dense_w(1568, vector<double>(120, 0));
+    vector<double> enc_dense_b(120, 0);
+    vector<double> enc_dense_sum(120, 0);
+    vector<double> enc_dense_sigmoid(120, 0);
+    vector<vector<double>> enc_dense_w2(120, vector<double>(10, 0));
+    vector<double> enc_dense_b2(10, 0);
+    vector<double> enc_dense_sum2(10, 0);
+    vector<double> enc_dense_softmax(10, 0);
+
+    vector<vector<double>> enc_dw2(120, vector<double>(10, 0));
+    vector<double> enc_db2(10, 0);
+    vector<vector<double>> enc_dw1(1568, vector<double>(120, 0));
+    vector<double> enc_db1(120, 0);
+
+    vector<vector<vector<double>>> enc_dw_max(8, vector<vector<double>>(28, vector<double>(28, 0)));
+    vector<vector<vector<double>>> enc_dw_conv(8, vector<vector<double>>(5, vector<double>(5, 0)));
+    vector<vector<vector<double>>> enc_db_conv(8, vector<vector<double>>(28, vector<double>(28, 0)));
+
+    /* ************************************************************ */
+
     // create a socket
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd == -1) 
@@ -93,6 +130,130 @@ int main()
                 k++;
                 memset(indata, 0, sizeof(indata));
             } while (data_size > 0);
+
+            data_size = 8 * 5 * 5;
+            k = 0;
+            do
+            {
+                int nbytes = recv(new_fd, indata, sizeof(indata), 0);
+                if (nbytes <= 0) 
+                {
+                    close(new_fd);
+                    printf("client closed connection.\n");
+                    break;
+                }
+                enc_conv_w[k / 25][(k / 5) % 5][k % 5] = atof(indata);
+                data_size--;
+                k++;
+                memset(indata, 0, sizeof(indata));
+            } while (data_size > 0);
+            
+            data_size = 8 * 28 * 28;
+            k = 0;
+            do
+            {
+                int nbytes = recv(new_fd, indata, sizeof(indata), 0);
+                if (nbytes <= 0) 
+                {
+                    close(new_fd);
+                    printf("client closed connection.\n");
+                    break;
+                }
+                enc_conv_b[k / 784][(k / 28) % 28][k % 28] = atof(indata);
+                data_size--;
+                k++;
+                memset(indata, 0, sizeof(indata));
+            } while (data_size > 0);
+
+            // Convolution Operation
+            for (int filter_dim = 0; filter_dim < 8; filter_dim++) 
+            {
+                for (int i = 0; i < 28; i++) 
+                {
+                    for (int j = 0; j < 28; j++) 
+                    {
+                        for (int k = 0; k < filter_size; k++) 
+                        {
+                            for (int l = 0; l < filter_size; l++) 
+                            {
+                                enc_conv_layer[filter_dim][i][j] = enc_img[i + k][j + l] * enc_conv_w[filter_dim][k][l];
+                            }
+                        }
+                        enc_conv_layer[filter_dim][i][j] = enc_conv_layer[filter_dim][i][j] + enc_conv_b[filter_dim][i][j];
+                    }
+                }
+            }
+            
+            data_size = 8 * 28 * 28;
+		    k = 0;
+            do
+            {
+                sprintf(outdata, "%f", enc_conv_layer[k / 784][(k / 28) % 28][k % 28]);
+                send(new_fd, outdata, sizeof(outdata), 0);
+                data_size--;
+                k++;
+                memset(outdata, 0, sizeof(outdata));
+            } while (data_size > 0);
+
+            data_size = 8 * 28 * 28;
+            k = 0;
+            do
+            {
+                int nbytes = recv(new_fd, indata, sizeof(indata), 0);
+                if (nbytes <= 0) 
+                {
+                    close(new_fd);
+                    printf("client closed connection.\n");
+                    break;
+                }
+                enc_enc_sig_layer[k / 784][(k / 28) % 28][k % 28] = atof(indata);
+                data_size--;
+                k++;
+                memset(indata, 0, sizeof(indata));
+            } while (data_size > 0);
+
+            // MAX Pooling (max_pooling, max_layer)
+            double cur_max = 0;
+            int max_i = 0, max_j = 0;
+            for (int filter_dim = 0; filter_dim < 8; filter_dim++) 
+            {
+                for (int i = 0; i < 28; i += 2) 
+                {
+                    for (int j = 0; j < 28; j += 2) 
+                    {
+                        max_i = i;
+                        max_j = j;
+                        cur_max = enc_sig_layer[filter_dim][i][j];
+                        for (int k = 0; k < 2; k++) 
+                        {
+                            for (int l = 0; l < 2; l++) 
+                            {
+                                if (enc_sig_layer[filter_dim][i + k][j + l] > cur_max) {
+                                    max_i = i + k;
+                                    max_j = j + l;
+                                    cur_max = enc_sig_layer[filter_dim][max_i][max_j];
+                                }
+                            }
+                        }
+                        enc_max_pooling[filter_dim][max_i][max_j] = 1;
+                        enc_max_layer[filter_dim][i / 2][j / 2] = cur_max;
+                    }
+                }
+            }
+
+            int k = 0;
+            for (int filter_dim = 0; filter_dim < 8; filter_dim++) 
+            {
+                for (int i = 0; i < 14; i++) 
+                {
+                    for (int j = 0; j < 14; j++) 
+                    {
+                        enc_dense_input[k] = enc_max_layer[filter_dim][i][j];
+                        k++;
+                    }
+                }
+            }
+            
             auto endTime = chrono::high_resolution_clock::now();
             chrono::duration<double, milli> elapsed = endTime - startTime;
 
@@ -101,17 +262,8 @@ int main()
             int elapsedSeconds_ = static_cast<int>(elapsedSeconds) % 60;
             int elapsedMilliseconds = static_cast<int>(elapsedSeconds * 1000) % 1000;
             cout << fixed << setprecision(10);
-            cout << "Training time : " << elapsedMinutes << "m " << elapsedSeconds_ << "s " << elapsedMilliseconds << "ms";
+            cout << "Transmission time : " << elapsedMinutes << "m " << elapsedSeconds_ << "s " << elapsedMilliseconds << "ms";
             cout << endl;
-            for (int i = 0; i < 32; i++)
-            {
-                for (int j = 0; j < 32; j++)
-                {
-                    cout << setw(3) << enc_img[i][j];
-                }
-                cout << endl;
-            }
-            cout << "k: " << k << endl;
             
             int nbytes = recv(new_fd, indata, sizeof(indata), 0);
             if (nbytes <= 0) 
